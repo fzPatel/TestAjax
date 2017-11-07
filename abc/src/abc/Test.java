d21f5d6bffaec75529a2910ed028dfd8931e4b30
Workplace <Workplace@Hp>
2017-11-07 13:44:20 +0530

    First Check

---
+++ abc/src/abc/Test.java
@@ -0,0 +1,30 @@
+package abc;
+
+public class Test {
+
+	
+	public static void main(String []args) 
+	{
+	int x,y;
+	for(x=0;x<5;x++)
+	{
+		System.out.println(" ");
+	
+		for(y=0;y>x;y--) {
+			
+			System.out.println(y);
+
+		}
+		
+	}		
+		
+		}
+		
+		
+		
+		
+		
+	}
+	
+	
+
